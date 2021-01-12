9fe50ba8a3dcdd78beb115db35b96a702c5f6af4
Marcus Borkenhagen <m@fritschy.de>
2021-01-12 14:29:52 +0100

    add docker/ and scripts/

---
+++ scripts/deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+docker run -u $(id -u) -v $HOME:$HOME -v /data:/data -w $PWD -it -e HOME=$HOME -e RUSTFLAGS= m-buster cargo br
+tar -c handlers password.txt nag-*.txt -C target/release zebot | ssh apu "tar -C zebot -xv"
