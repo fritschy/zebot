ed5c9be0b99a8dde2652ec714003e10d052a6702
Marcus Borkenhagen <m@fritschy.de>
2021-01-29 11:49:59 +0100

    irc2: reformat parser

--- src/irc2/parser.rs
+++ src/irc2/parser.rs
@@ -1,45 +1,47 @@
-use nom::{
-    IResult,
-    bytes::complete::{
-        tag,
-        take_until,
-        take_while,
-        take_while_m_n,
-    },
-    multi::{
-        count,
-        many_till,
-    },
-    character::{
-        is_alphabetic,
-        is_digit,
-    },
-    error::Error,
-    combinator::{
-        opt,
-        map,
-        eof,
-    },
-    branch::alt
-};
+use nom::IResult;
 
-use std::fmt::{Display, Formatter};
-use std::borrow::Cow;
-use nom::bytes::complete::take_till1;
-use nom::character::complete::{char, space1, anychar, one_of};
-use nom::multi::{many0, many_m_n};
-
-pub fn parse<'a>(i: &'a[u8]) -> IResult<&'a[u8], ()> {
+pub fn parse<'a>(i: &'a [u8]) -> IResult<&'a [u8], ()> {
     parsers::message(i)
 }
 
 mod parsers {
-    use super::*;
-    use futures_util::StreamExt;
-    use nom::multi::many1;
-    use nom::character::complete::none_of;
-    use crate::irc2::parser::parsers::utils::{string_plus_char, string_from_parts};
+    use nom::{
+        branch::alt,
+        bytes::complete::{
+            tag,
+            take_till1,
+            take_until,
+            take_while,
+            take_while_m_n,
+        },
+        character::{
+            complete::{
+                anychar, char, none_of, one_of,
+                space1,
+            },
+            is_alphabetic,
+            is_digit,
+        },
+        combinator::{
+            eof,
+            map,
+            opt,
+        },
+        error::Error,
+        IResult,
+        multi::{
+            count,
+            many0,
+            many1,
+            many_m_n,
+            many_till,
+        },
+    };
     use nom::number::complete::be_u8;
+
+    use crate::irc2::parser::parsers::utils::{string_from_parts, string_plus_char};
+
+    use super::*;
 
     mod utils {
         pub fn string_from_parts(first: char, rest: &Vec<char>) -> String {
@@ -57,7 +59,7 @@ mod parsers {
     }
 
     // rfc2812.txt:321
-    pub fn message<'a>(i: &'a[u8]) -> IResult<&'a[u8], ()> {
+    pub fn message<'a>(i: &'a [u8]) -> IResult<&'a [u8], ()> {
         let (i, prefix) = opt(parsers::prefix)(i)?;
         let (i, command) = parsers::command(i)?;
         let (i, p) = opt(params)(i)?;
@@ -66,31 +68,31 @@ mod parsers {
     }
 
     // rfc2812.txt:329
-    pub fn middle<'a>(i:&'a[u8]) -> IResult<&'a[u8], String> {
+    pub fn middle<'a>(i: &'a [u8]) -> IResult<&'a [u8], String> {
         let (i, first) = nospcrlfcl()(i)?;
         let (i, rest) = many0(alt((char(':'), nospcrlfcl())))(i)?;
         Ok((i, string_from_parts(first, &rest)))
     }
 
     // rfc2812.txt:324
-    pub fn params<'a>(i:&'a[u8]) -> IResult<&'a[u8], String> {
+    pub fn params<'a>(i: &'a [u8]) -> IResult<&'a [u8], String> {
         Ok((i, String::new()))
     }
 
     // rfc2812.txt:327
-    pub fn nospcrlfcl<'a>() -> impl FnMut(&'a[u8]) -> IResult<&'a[u8], char> {
+    pub fn nospcrlfcl<'a>() -> impl FnMut(&'a [u8]) -> IResult<&'a [u8], char> {
         none_of("\0\x13\x10 :")
     }
 
     // rfc2812.txt:322
-    pub fn prefix<'a>(i:&'a[u8]) -> IResult<&'a[u8], String> {
+    pub fn prefix<'a>(i: &'a [u8]) -> IResult<&'a [u8], String> {
         let (i, _) = char(':')(i)?;
         let (i, servnick) = alt((nickname_part, servername()))(i)?;
         let (i, _) = char(' ')(i)?;
         Ok((i, servnick))
     }
 
-    pub fn nickname_part<'a>(i:&'a[u8]) -> IResult<&'a[u8], String> {
+    pub fn nickname_part<'a>(i: &'a [u8]) -> IResult<&'a [u8], String> {
         let (i, nick) = nickname(i)?;
         let (i, excl) = opt(char('!'))(i)?;
         if let Some(excl) = excl {
@@ -106,11 +108,11 @@ mod parsers {
         Ok((i, nick))
     }
 
-    pub fn servername<'a>() -> impl FnMut(&'a[u8]) -> IResult<&'a[u8], String> {
+    pub fn servername<'a>() -> impl FnMut(&'a [u8]) -> IResult<&'a [u8], String> {
         alt((ip4addr, ip6addr))
     }
 
-    pub fn ip4addr<'a>(i:&'a[u8]) -> IResult<&'a[u8], String> {
+    pub fn ip4addr<'a>(i: &'a [u8]) -> IResult<&'a [u8], String> {
         let (i, a) = be_u8(i)?;
         let (i, _) = char('.')(i)?;
         let (i, b) = be_u8(i)?;
@@ -122,27 +124,27 @@ mod parsers {
         Ok((i, format!("{}.{}.{}.{}", a, b, c, d)))
     }
 
-    pub fn ip6addr<'a>(i:&'a[u8]) -> IResult<&'a[u8], String> {
+    pub fn ip6addr<'a>(i: &'a [u8]) -> IResult<&'a [u8], String> {
         // FIXME
         Ok((i, String::new()))
     }
 
-    pub fn host<'a>() -> impl FnMut(&'a[u8]) -> IResult<&'a[u8], String> {
+    pub fn host<'a>() -> impl FnMut(&'a [u8]) -> IResult<&'a [u8], String> {
         alt((hostname, hostaddr))
     }
 
-    pub fn hostaddr<'a>(i:&'a[u8]) -> IResult<&'a[u8], String> {
+    pub fn hostaddr<'a>(i: &'a [u8]) -> IResult<&'a [u8], String> {
         Ok((i, String::new()))
     }
 
-    pub fn hostname<'a>(i:&'a[u8]) -> IResult<&'a[u8], String> {
+    pub fn hostname<'a>(i: &'a [u8]) -> IResult<&'a [u8], String> {
         let (i, first) = shortname(i)?;
         let (i, dot) = many0(dot_prefixed(shortname))(i)?;
         Ok((i, first + &dot.into_iter().collect::<String>()))
     }
 
-    pub fn dot_prefixed<'a>(p: impl Fn(&'a[u8])->IResult<&'a[u8], String>) -> impl Fn(&'a[u8]) -> IResult<&'a[u8], String> {
-        move |i:&[u8]| {
+    pub fn dot_prefixed<'a>(p: impl Fn(&'a [u8]) -> IResult<&'a [u8], String>) -> impl Fn(&'a [u8]) -> IResult<&'a [u8], String> {
+        move |i: &[u8]| {
             let (i, dot) = char('.')(i)?;
             let (i, rest) = p(i)?;
             let res = String::from(dot) + rest.as_str();
@@ -150,13 +152,13 @@ mod parsers {
         }
     }
 
-    pub fn shortname<'a>(i:&'a[u8]) -> IResult<&'a[u8], String> {
+    pub fn shortname<'a>(i: &'a [u8]) -> IResult<&'a [u8], String> {
         let (i, first) = alt((letter(), digit()))(i)?;
         let (i, rest) = many0(alt((letter(), digit(), char('-'))))(i)?;
         Ok((i, utils::string_from_parts(first, &rest)))
     }
 
-    pub fn command<'a>(i:&'a[u8]) -> IResult<&'a[u8], String> {
+    pub fn command<'a>(i: &'a [u8]) -> IResult<&'a [u8], String> {
         let (i, cmd) = alt((
             take_while_m_n(3, 3, is_digit),
             take_till1(|c| c == b' ')))(i)?;
@@ -165,25 +167,25 @@ mod parsers {
         Ok((i, cmd))
     }
 
-    pub fn user<'a>() -> impl FnMut(&'a[u8]) -> IResult<&'a[u8], String> {
+    pub fn user<'a>() -> impl FnMut(&'a [u8]) -> IResult<&'a [u8], String> {
         map(many1(none_of("\0\x13\x10 @")), |x| x.into_iter().collect::<String>())
     }
 
-    pub fn nickname<'a>(i:&'a[u8]) -> IResult<&'a[u8], String> {
+    pub fn nickname<'a>(i: &'a [u8]) -> IResult<&'a [u8], String> {
         let (i, first) = alt((letter(), special()))(i)?;
         let (i, mut rest) = many_m_n(0, 8, alt((letter(), digit(), special(), char('-'))))(i)?;
         Ok((i, utils::string_from_parts(first, &rest)))
     }
 
-    pub fn digit<'a>() -> impl Fn(&'a[u8]) -> IResult<&'a[u8], char> {
+    pub fn digit<'a>() -> impl Fn(&'a [u8]) -> IResult<&'a [u8], char> {
         one_of("0123456789")
     }
 
-    pub fn letter<'a>() -> impl Fn(&'a[u8]) -> IResult<&'a[u8], char> {
+    pub fn letter<'a>() -> impl Fn(&'a [u8]) -> IResult<&'a [u8], char> {
         one_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
     }
 
-    pub fn special<'a>() -> impl Fn(&'a[u8]) -> IResult<&'a[u8], char> {
+    pub fn special<'a>() -> impl Fn(&'a [u8]) -> IResult<&'a [u8], char> {
         one_of("\x5b\x5c\x5d\x5e\x5f\x60\x7b\x7c\x7d[]\\`_^{|}")
     }
 }
